d55df0d59f170e6b7052c24478a633e0ded6c14a
Jere Pehkonen <jere.a.pehkonen@student.jyu.fi>
2020-03-19 16:15:17 +0200

    Add expression parsing tests

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -34,3 +34,31 @@ spec = do
     it "parses as void" $
       parse tpe "" "void" `shouldParse` CVoid
 
+-- expressions
+
+  let ans = Subtr (Add (Mul (IntConst 2) (IntConst 3)) (IntConst 2))
+                     (Div (IntConst 33) (IntConst 25))
+  describe "expr with arith ops" $ do
+    it "should parse with correct precedence" $
+      parse parseExpr "" "2*3+2-33/25" `shouldParse` ans
+
+  let ans = Assign (Var "a") (Add (IntConst 3) (IntConst 2))
+  describe "expr with assignment" $ do
+    it "should parse with assignment as lowest prec" $
+      parse parseExpr "" "a=3+2" `shouldParse` ans
+
+  let ans = Lt (Add (IntConst 2) (IntConst 3)) (IntConst 4)
+  describe "expr with comp ops" $ do
+    it "should parse comparisons as lower prec than arith ops" $
+      parse parseExpr "" "2 + 3 < 4" `shouldParse` ans
+
+  let ans = Assign (Var "a") (Assign (Var "b") (Assign (Var "c") (IntConst 4)))
+  describe "expr with multiple assignments" $ do
+    it "should parse assignments as right assoc" $
+      parse parseExpr "" "a=b=c=4" `shouldParse` ans
+
+  let ans = Mul (Add (IntConst 2) (IntConst 2)) (Add (IntConst 3) (IntConst 1))
+  describe "expr with parentheses and extra spaces in between and end" $ do
+    it "should parse correctly" $
+      parse (parseExpr <* eof) "" "(2+2) * (3+1)  " `shouldParse` ans
+
