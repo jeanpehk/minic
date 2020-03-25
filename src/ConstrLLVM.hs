ddcd312957b06294b930df147348fcefcb3d7b8f
Jere Pehkonen <jere.a.pehkonen@student.jyu.fi>
2020-03-25 10:48:21 +0200

    Better infrastructure for llvm gen, now can use symtable with state

--- src/ConstrLLVM.hs
+++ src/ConstrLLVM.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module ConstrLLVM where
 
@@ -11,71 +12,87 @@ import Data.Word
 
 import qualified AST as Mc
 
-import Checker
-import Control.Monad.Except
 import Control.Monad.State
 import qualified Data.Map as Map
 
 import LLVM.Pretty
-import qualified LLVM.AST as IR
-import qualified LLVM.AST.Name as IR
-import qualified LLVM.AST.Constant as IR
+import qualified LLVM.AST as AST
+import qualified LLVM.AST.Name as AST
+import qualified LLVM.AST.Constant as AST
+import qualified LLVM.IRBuilder.Constant as IR
+import qualified LLVM.IRBuilder.Instruction as IR
 import qualified LLVM.IRBuilder.Module as IR
+import LLVM.IRBuilder.Monad
 
--- This module constructs the llvm module
+-- This module constructs the Haskell LLVM AST representation.
 
 ------------------------------------------------------------
 -- Datatypes
 ------------------------------------------------------------
 
-type Codegen a = Checker a
+type Generator a = IR.ModuleBuilderT (State Names) a
 
-type GenState a = (State Gen a)
-
-data Gen
-  = Gen { irblocks :: [Map.Map Block IR.Name]
-        , env      :: [ST] }
-  deriving (Eq, Show)
-
-data Block
-  = Block { idx :: Int
-          , stack :: [IR.Named IR.Instruction] }
-  deriving (Eq, Show)
-
-newtype ST = ST (Map.Map String IR.Operand)
+newtype Names = Names { getNames :: [Map.Map String Int] }
   deriving (Eq, Show)
 
 ------------------------------------------------------------
 -- Functions for building an LLVM Module
 ------------------------------------------------------------
 
-int32 :: IR.Type
-int32 = IR.IntegerType 32
+int32 :: AST.Type
+int32 = AST.IntegerType 32
 
 -- Change type from own AST into LLVM
-decideType :: Mc.Type -> IR.Type
+decideType :: Mc.Type -> AST.Type
 decideType Mc.CInt = int32
 
---runCodegen :: Mc.TUnit -> Env -> (Either Error IR.Module, Env)
---runCodegen tunit = (runState . runExceptT) (codeGen tunit)
-
-runGen :: Mc.TUnit -> (IR.Module, Gen)
-runGen tunit = runState (codeGen tunit) gen
+runGen tunit = runState (IR.buildModuleT "ok" (codeGen tunit)) ns
   where
-    gen = Gen { irblocks = [], env = [] }
+    ns = Names [Map.empty]
 
+test :: IO ()
+test = do
+  let res = snd $ runGen (Mc.TUnit [(Mc.GDecl (Mc.Decl Mc.CInt "hello"))
+                            , (Mc.FDef (Mc.Func Mc.CInt "func"
+                                       [Mc.Param Mc.CInt "ok"] (Mc.Block [])))])
+  Prelude.putStrLn $ show res
 
-codeGen :: Mc.TUnit -> GenState IR.Module
-codeGen (Mc.TUnit tls) = IR.buildModuleT "minic.ll" $ mdo
+------------------------------------------------------------
+-- Generation
+------------------------------------------------------------
+
+-- Translation units
+codeGen :: Mc.TUnit -> Generator [AST.Operand]
+codeGen (Mc.TUnit tls) = do
   let x = \y -> case y of
                   (Mc.GDecl decl) -> genDecl decl
                   (Mc.FDef func)  -> genFunc func
+  ns <- get
+  put $ Names $ (Map.insert "maintypestuff" 0 Map.empty):(getNames ns)
   mapM x tls
 
-genDecl :: IR.MonadModuleBuilder m => Mc.Decl -> m IR.Operand
-genDecl (Mc.Decl (Mc.CInt) id) = 
-  IR.global (IR.mkName id) int32 (IR.Int (fromIntegral 32) 0)
+-- Top level declarations
+genTl (Mc.GDecl (Mc.Decl Mc.CInt id)) = do
+  IR.global (AST.mkName id) int32 (AST.Int (fromIntegral 32) 0)
 
-genFunc :: IR.MonadModuleBuilder m => Mc.Func -> m IR.Operand
-genFunc (Mc.Func tpe id params block) = undefined
+-- Declarations
+genDecl :: Mc.Decl -> Generator AST.Operand
+genDecl (Mc.Decl Mc.CInt id) = do
+  ns <- get
+  put $ Names $ (Map.insert "DECLSDECLSDECLs" 0 Map.empty):(getNames ns)
+  IR.global (AST.mkName id) int32 (AST.Int (fromIntegral 32) 0)
+
+-- Functions
+genFunc :: Mc.Func -> Generator AST.Operand
+genFunc (Mc.Func tpe id params block) = mdo
+  let ps = map genParam params
+  ns <- get
+  put $ Names $ (Map.insert "FUNCITUP" 0 Map.empty):(getNames ns)
+  IR.function (AST.mkName id) ps (decideType tpe) $ \ops -> mdo
+      IR.alloca int32 Nothing 32
+      IR.ret (head ops)
+
+-- Parameters
+genParam :: Mc.Param -> (AST.Type, IR.ParameterName)
+genParam (Mc.Param tpe id) = (decideType tpe, IR.ParameterName (fromString id))
 
